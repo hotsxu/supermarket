ee40ba422e74cdb37d4f78ced721a788e4c6df92
hotsxu <hotsxu@gmail.com>
2018-06-26 20:12:26 +0800

    add some UI

---
+++ app/src/main/java/com/pemt/market/flow/MainRecyclerAdapter.kt
@@ -0,0 +1,13 @@
+package com.pemt.market.flow
+
+import android.content.Context
+import com.pemt.market.R
+import com.pemt.market.app.BaseRecyclerAdapter
+import com.pemt.market.app.MyRecyclerViewHolder
+
+class MainRecyclerAdapter(context: Context) : BaseRecyclerAdapter<MyRecyclerViewHolder>(context, R.layout.item_main) {
+    override fun convert(holder: MyRecyclerViewHolder, point: Int, t: MyRecyclerViewHolder) {
+
+    }
+
+}
